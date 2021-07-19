12595a204b8068e7b02a87875982229fdc00efd8
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-07-19 09:32:23 -0700

    setup of mysql database connection

--- data_retrieval.js
+++ data_retrieval.js
@@ -1,35 +1,68 @@
 // Establish DB Connection
-var mysql = require('mysql');
-var express = require('express');
-var display = express();
+const mysql = require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+app.use(bodyparser.json());
+
 var con = mysql.createConnection({
     host:'localhost',
     user: 'root',
     password: 'ThePassword',
     database: 'online_portfolio'
 });
+//check connection
+con.connect((err)=>{
+    if(!err)
+        console.log("Connection success!");
+    else
+        console.log("Connection failure: \n Error:"+ JSON.stringify(err,undefined,2));
+});
+app.listen(3000,()=>console.log('Server running at port number 3000'));
 
-//get reqs
-//about biography
-display.get('aboutMe', function(request, response){
+//About Me
+app.get('/aboutMe', function(request, response){
     console.log('GET req success: aboutMe for About Me')
-    con.query("SELECT Synopsis FROM About", function (err, result){
+    //get about me biography
+    con.query("SELECT Synopsis FROM About", (error, synopsis)=> {
+        if (error) throw error;
+        else{
+            response.send(synopsis);
+            console.log("Synopsis retrieved.");
+        }
+    });
+});
+//get work experience
+app.get('/workEx', function(req,res){
+    console.log('GET req success: workEx for About Me')
+    con.query("SELECT * FROM WorkExperience", (err, workex)=>{
         if (err) throw err;
         else{
-            response.send(result)
+            res.send(workex);
+            console.log("Work experience retrieved.");
+        }
+    });
+});
+//get coursework
+app.get('/coursework', function(req,res){
+    console.log('GET req success: Coursework for About Me')
+    con.query("SELECT * FROM Courses", (e, courses)=>{
+        if (e) throw e;
+        else{
+            res.send(courses);
+            console.log("Coursework retrieved.");
         }
     });
 });
 
-//project titles and descriptions
-display.get('projectsDesc', function(request, response){
+//get all project titles and descriptions
+app.get('/projectsDesc', function(request, response){
     console.log('GET req success: projectsDesc for Portfolio')
     con.query("SELECT * FROM Projects", function (error, result){
         if (error) throw error;
         else{
-            response.send(result)
+            response.send(result);
+            console.log("Project details retrieved.")
         }
     });
 });
-
-
