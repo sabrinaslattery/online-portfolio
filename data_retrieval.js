ca89daaaf83c92932246e9b7ca46ec0514df41ec
Sabrina Slattery <sabrina.slattery.noreply@gmail.com>
2021-07-19 11:30:11 -0700

    cosmetic changes, hold off on DB data rendering

--- data_retrieval.js
+++ data_retrieval.js
@@ -2,8 +2,6 @@
 const mysql = require('mysql');
 const express = require('express');
 var app = express();
-const bodyparser = require('body-parser');
-app.use(bodyparser.json());
 
 var con = mysql.createConnection({
     host:'localhost',
@@ -11,6 +9,7 @@ var con = mysql.createConnection({
     password: 'ThePassword',
     database: 'online_portfolio'
 });
+
 //check connection
 con.connect((err)=>{
     if(!err)
@@ -18,21 +17,40 @@ con.connect((err)=>{
     else
         console.log("Connection failure: \n Error:"+ JSON.stringify(err,undefined,2));
 });
+//establish port
 app.listen(3000,()=>console.log('Server running at port number 3000'));
 
-//About Me
+// Set up HTML views
+app.set('view engine', 'pug');
+const url = require('url');
+const webURL = new URL('https://sabrinaslattery.github.io/online-portfolio/index.html');
+
+//Access css files and images
+app.use(express.static('../Online-portfolio'));
+app.use(express.static('../Online-portfolio/portfolio_images'));
+
+//Get page data
+app.get('/', function(req, res){
+    res.sendFile('PortfolioAboutMe.html', {root: __dirname})
+});
+app.use(express.json());
+app.use(express.json({extended:false}));
+
+//Get About Me Synopsis
 app.get('/aboutMe', function(request, response){
     console.log('GET req success: aboutMe for About Me')
-    //get about me biography
     con.query("SELECT Synopsis FROM About", (error, synopsis)=> {
         if (error) throw error;
         else{
             response.send(synopsis);
+            response.render('/synopsis',{synopsis: synopsis});
             console.log("Synopsis retrieved.");
         }
     });
 });
-//get work experience
+
+
+//Get work experience
 app.get('/workEx', function(req,res){
     console.log('GET req success: workEx for About Me')
     con.query("SELECT * FROM WorkExperience", (err, workex)=>{
@@ -66,3 +84,11 @@ app.get('/projectsDesc', function(request, response){
         }
     });
 });
+
+
+//rendering
+const http = require('http');
+
+function onRequest(req, res) {
+
+};
